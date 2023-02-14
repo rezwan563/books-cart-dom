9c0d259ac2f541ab6e6e1b097b23ea18e3a452a8
Rezwan <t.rewan17@gmail.com>
2023-02-14 20:55:14 +0600

    Added quantity and order items functionality

--- js/cart.js
+++ js/cart.js
@@ -15,17 +15,23 @@ function getReceipt(bookTitle, quantity, totalPrice){
     const bookQuantity = getQuantityFromField(quantity);
     // const bookPrice = getPriceFromtext(priceField);
     
-    return `${bookName} - ${bookQuantity}pc - ${totalPrice} `;
+    return `${bookName} - ${bookQuantity}pc - ${totalPrice} tk`;
 
 }
-function writeAddedItems(inputId, quantity, totalPrice){
+function writeAddedItems(receiptText){
+    
     const writeArea = document.getElementById("order-item");
     const newElement = document.createElement("p");
-    newElement.innerText = (getReceipt(inputId, quantity, totalPrice));
+    newElement.innerText = receiptText;
     writeArea.append(newElement);
-    console.log(newElement);
     
 }
+
+// function removeFromCart(e){
+//     document.getElementById(e.).parentElement.
+
+    
+// }
 
 
 
@@ -33,31 +39,47 @@ function writeAddedItems(inputId, quantity, totalPrice){
 
 
 document.getElementById('sapiens-btn').addEventListener('click', function(){
+    // const inputField = document.getElementById('sapiens-field').value;
+    // console.log(inputField);
     const sapiensQuantity = getQuantityFromField("sapiens-field");
     const sapiensUnitPrice = getPriceFromtext("sapiens-price");
     const sapiensTotal = sapiensQuantity * sapiensUnitPrice;
     const receipt = getReceipt('sapiens-title', 'sapiens-field', sapiensTotal);
-    console.log(receipt);
-    writeAddedItems("")
+    writeAddedItems(receipt);
+
 })
 document.getElementById('meditation-btn').addEventListener('click', function(){
     const meditationQuantity = getQuantityFromField("meditation-field");
-    console.log(meditationQuantity);
-    console.log('button click')
+    const meditationUnitPrice = getPriceFromtext("meditation-price");
+    const meditationTotal = meditationQuantity * meditationUnitPrice;
+    const receipt = getReceipt('meditation-title', 'meditation-field', meditationTotal);
+    writeAddedItems(receipt);
 })
 document.getElementById('mans-search-btn').addEventListener('click', function(){
     const mansQuantity = getQuantityFromField("mans-search-field");
-    console.log(mansQuantity);
+    const mansSearchUnitPrice = getPriceFromtext("mans-search-price");
+    const mansSearchTotal = mansQuantity * mansSearchUnitPrice;
+    const receipt = getReceipt('mans-search-title', 'mans-search-field', mansSearchTotal);
+    writeAddedItems(receipt);
 })
 document.getElementById('nations-fail-btn').addEventListener('click', function(){
-    const nationsQuantity = getQuantityFromField("nations-fail-field");
-    console.log(nationsQuantity);
+    const nationsFailQuantity = getQuantityFromField("nations-fail-field");
+    const nationsFailUnitPrice = getPriceFromtext("nations-fail-price");
+    const nationsFailTotal = nationsFailQuantity * nationsFailUnitPrice;
+    const receipt = getReceipt('nations-fail-title', 'nations-fail-field', nationsFailTotal);
+    writeAddedItems(receipt);
 })
 document.getElementById('ugly-truth-btn').addEventListener('click', function(){
     const uglyTruthQuantity = getQuantityFromField("ugly-truth-field");
-    console.log(uglyTruthQuantity);
+    const uglyTruthUnitPrice = getPriceFromtext("ugly-truth-price");
+    const uglyTruthTotal = uglyTruthQuantity * uglyTruthUnitPrice;
+    const receipt = getReceipt('ugly-truth-title', 'ugly-truth-field', uglyTruthTotal);
+    writeAddedItems(receipt);
 })
 document.getElementById('born-crime-btn').addEventListener('click', function(){
     const bornCrimeQuantity = getQuantityFromField("born-crime-field");
-    console.log(bornCrimeQuantity);
+    const bornCrimeUnitPrice = getPriceFromtext("born-crime-price");
+    const bornCrimeTotal = bornCrimeQuantity * bornCrimeUnitPrice;
+    const receipt = getReceipt('born-crime-title', 'born-crime-field', bornCrimeTotal);
+    writeAddedItems(receipt);
 })
